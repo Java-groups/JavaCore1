5d5b4fcabd8a8f489441c46cc6e4bff1817289ff
Oleksii-GitRepere <oleksii485th@gmail.com>
2022-12-04 10:59:23 +0200

    Oleksii_HOME_WORK_2_fillout_list

--- SoftServe_HW/src/com/home_work_2_oop_intro/App.java
+++ SoftServe_HW/src/com/home_work_2_oop_intro/App.java
@@ -9,60 +9,51 @@ public class App
 
 
 
-		/*
-
-		//creating Person instances and fill out info with constructor of class and method setBirthYear()
-
-		Person p1 = new Person("John","Doe");
-		p1.setBirthYear(1980);
-		p1.changeName("Chris", "Davies");
-		p1.output();
-
-		Person p2 = new Person("Connor","Smith");
-		p2.setBirthYear(1990);
-		p2.output();
-
-		Person p3 = new Person("Robert","Stahowski");
-		p3.setBirthYear(1992);
-		p3.output();
-
-		Person p4 = new Person("Jeremi","Thomas");
-		p4.setBirthYear(1994);
-		p4.output();
-
-		Person p5 = new Person("Osmond ","Hughes");
-		p5.setBirthYear(1996);
-		p5.output();
-
-		*/
-
-
-
-		//creating class Person instances with constructor and fill out person info with method input()
+		
+		//creating class Person instances with constructor and fill out person info with method fillOutPersonList()
 		Scanner in_scanner_1 = new Scanner(System.in);
 
-		Person p1 = new Person();
-		Person p2 = new Person();
-		Person p3 = new Person();
-		Person p4 = new Person();
-		Person p5 = new Person();
 
-		p1.input(in_scanner_1);
-		p1.output();
-		p2.input(in_scanner_1);
-		p2.output();
-		p3.input(in_scanner_1);
-		p3.output();
-		p4.input(in_scanner_1);
-		p4.output();
-		p5.input(in_scanner_1);
-		p5.output();
+		Person person_list = new Person();
+		person_list.fillOutPersonList(in_scanner_1);
+		person_list.out();
+
 
 		in_scanner_1.close();
 
 
 
 
+		
+		/*
+		
+		//-----------------------------------------------------------------------------------------------
+		//creating Person instances and fill out info with constructor of class and method setBirthYear()
+		Person p1 = new Person("John");
+		p1.setLastName("Doe");
+		p1.setBirthYear(1980);
+		p1.setFirstName("Chris");
+		p1.setLastName("Davies");
+		p1.output();
+		Person p2 = new Person("Connor");
+		p2.setLastName("Smith");
+		p2.setBirthYear(1990);
+		p2.output();
+		Person p3 = new Person("Robert");
+		p3.setLastName("Stahowski");
+		p3.setBirthYear(1992);
+		p3.output();
+		Person p4 = new Person("Jeremi");
+		p4.setLastName("Thomas");
+		p4.setBirthYear(1994);
+		p4.output();
+		Person p5 = new Person("Osmond");
+		p5.setLastName("Hughes");
+		p5.setBirthYear(1996);
+		p5.output();
+		
+		
+		*/
 
 	}
 }
--- SoftServe_HW/src/com/home_work_2_oop_intro/Person.java
+++ SoftServe_HW/src/com/home_work_2_oop_intro/Person.java
@@ -1,28 +1,35 @@
 package com.home_work_2_oop_intro;
 
+
+import java.util.HashMap;
 import java.util.Scanner;
+import java.time.Year;
 
 public class Person 
 {
 	private String firstName, 
-				   lastName;
+	   lastName;
 	private int  birthYear;
 
+	private static int count = 0;
+
+	private HashMap<Integer,Person> new_person_list = new HashMap<Integer,Person>();
 
 	public Person() 
 	{
-			
+		
 	}
 
-	public Person(String firstName, String lastName) 
+	public Person(String firstName) 
 	{
 		this.firstName = firstName;
-		this.lastName = lastName;
+		count++;
 	}
 
 	public Person(int birthYear) 
 	{
 		this.birthYear = birthYear;
+		count++;
 	}
 
 	public String getFirstName() 
@@ -50,14 +57,19 @@ public class Person
 		return birthYear;
 	}
 
+	public void setBirthYear(Scanner sc) 
+	{
+		this.birthYear = checkInFormatIntOrExit(sc);
+	}
+
 	public void setBirthYear(int birthYear) 
 	{
 		this.birthYear = birthYear;
 	}
-
+		
 	public int getAge() 
 	{
-		return 2022-this.birthYear;
+		return Year.now().getValue() - this.birthYear;
 	}
 
 	public void changeName(String fn, String ln)
@@ -65,7 +77,7 @@ public class Person
 		this.firstName = fn;
 		this.lastName = ln;
 	}
-	
+
 	public void input(Scanner in_sc)
 	{
 		System.out.println("Please, enter person first name: ");
@@ -74,25 +86,68 @@ public class Person
 		this.lastName = in_sc.next();
 		this.birthYear = checkInFormatIntOrExit(in_sc);
 	}
-
+		
+	//added new method for output info of persons in whole list of persons that was added
+	public void out() 
+	{
+		for (int i = 0; i < new_person_list.size(); i++) 
+		{
+			System.out.print((i + 1)+ ". First Name: " + new_person_list.get(Integer.valueOf(i)).getFirstName()+";");
+			System.out.print("\tLast Name: " + new_person_list.get(Integer.valueOf(i)).getLastName()+";");
+			System.out.print("\tBirth Year: " + new_person_list.get(Integer.valueOf(i)).getBirthYear()+";");
+			System.out.println("\tAge: " + new_person_list.get(Integer.valueOf(i)).getAge());
+		}
+	}
+		
+		
 	public void output() 
 	{
-		System.out.println("Person first name is: "+this.firstName);
-		System.out.println("Person last name is: "+this.lastName);
-		System.out.println("Person age is: "+this.getAge());
+			System.out.print("First Name: " + this.firstName);
+			System.out.print("\tLast Name: " + this.lastName);
+			System.out.print("\tBirth Year: " + this.birthYear);
+			System.out.println("\tAge: " + getAge());
 	}
-	
+
+
+	public void fillOutPersonList(Scanner sc)
+	{
+		boolean exit;
+		String  in_person_name;
+		do
+		{
+			System.out.println("Please, to fill out the persons list and add new person in it, enter Name and then LastName, or word `exit` to quit without changes.");
+			if("exit".equals(in_person_name=sc.next())) 
+			{
+				exit=true;
+			}
+			else
+			{
+				new_person_list.put(Integer.valueOf(Person.count),new Person(in_person_name));
+				System.out.println("Please, enter LastName of new person.");
+				new_person_list.get(Integer.valueOf(Person.count-1)).setLastName(sc.next());
+				new_person_list.get(Integer.valueOf(Person.count-1)).setBirthYear(sc);
+				exit=false;
+			}
+
+		}while(!exit);
+	}
+
+
 	private int checkInFormatIntOrExit(Scanner in)
 	{
 		String in_word;
-		 do 
+		do 
 		{
-			System.out.println("Please, enter person birth year, or q for exit.");
+			System.out.println("Please, enter person birth year, or `q` for exit.");
 			try 
 			{
-				if ("q".equals(in_word=in.next())) 
+				while( !"q".equals(in_word=in.next()) && Integer.parseInt(in_word)<=0 )
 				{
-					return 0;
+					System.out.println("Please, enter integer value greater then zero, or `q`.");					
+				}
+				if ("q".equals(in_word)) 
+				{
+					return -1;
 				}
 				else 
 				{
@@ -101,8 +156,9 @@ public class Person
 			}
 			catch(NumberFormatException exc)
 			{
-				System.out.println("Please, enter numeric value, or for exit enter: q.");
+				System.out.println("Please, enter integer value greater then zero, or for exit enter: q.");
 			}
 		}while(true); 
 	}
+
 }
