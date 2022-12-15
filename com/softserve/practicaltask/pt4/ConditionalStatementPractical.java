a2cd404d81f2ccee2a0519c6da3c8bff843860d6
olesmack <oles.mack@gmail.com>
2022-12-15 21:54:34 +0200

    Added enums

--- com/softserve/practicaltask/pt4/ConditionalStatementPractical.java
+++ com/softserve/practicaltask/pt4/ConditionalStatementPractical.java
@@ -10,9 +10,18 @@ public class ConditionalStatementPractical {
     private float c;
     private int i;
 
+    private int dayNumber;
+    private String dayName;
+    private String dayNameUA;
+    private String country;
+    private Continents continents;
+
+
     public static void main(String[] args) throws IOException {
         ConditionalStatementPractical cs = new ConditionalStatementPractical();
-        cs.oddNumbers();
+//        cs.oddNumbers();
+//        cs.dayOfTheWeek();
+        cs.continentCountry();
     }
 
     public void oddNumbers() throws IOException {
@@ -32,6 +41,49 @@ public class ConditionalStatementPractical {
         if (c % 2 == 0) {
             i++;
         }
-        System.out.println("Count of odd numbers is: " + i);
+        System.out.println("Count of odd numbers is: " + i + "\n");
+    }
+
+    public void dayOfTheWeek() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter day of the week number: ");
+        dayNumber = Integer.parseInt(br.readLine());
+        switch (dayNumber) {
+            case 1: dayName = "Monday"; dayNameUA = "Понеділок";
+                break;
+            case 2: dayName = "Tuesday"; dayNameUA = "Вівторок";
+                break;
+            case 3: dayName = "Wednesday"; dayNameUA = "Середа";
+                break;
+            case 4: dayName = "Thursday"; dayNameUA = "Четвер";
+                break;
+            case 5: dayName = "Friday"; dayNameUA = "П'ятниця";
+                break;
+            case 6: dayName = "Saturday"; dayNameUA = "Субота";
+                break;
+            case 7: dayName = "Sunday"; dayNameUA = "Неділя";
+                break;
+            default: dayName = "There is no day in the week with your number";
+        }
+        System.out.println("Your day name is: " + dayName + "\n" + dayNameUA);
+    }
+
+    public enum Continents {
+        EUROPE, ASIA, AFRICA, AUSTRALIA, NORTHAMERICA, SOUTHAMERICA, ANTARCTICA
+    }
+
+    public void continentCountry() throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter country name: ");
+        country = bf.readLine();
+        switch (country) {
+            case "Ukraine": continents = Continents.EUROPE;
+            break;
+            case "USA": continents = Continents.SOUTHAMERICA;
+            break;
+            default:
+                System.out.println("No such country in the world!");
+        }
+        System.out.println(continents);
     }
 }
