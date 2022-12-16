45be3e85219d1c6ed290986d070af4574fc1432a
olesmack <oles.mack@gmail.com>
2022-12-16 21:41:34 +0200

    Practical Product

---
+++ com/softserve/practicaltask/pt4/Product.java
@@ -0,0 +1,52 @@
+package com.softserve.practicaltask.pt4;
+
+public class Product {
+    private String name;
+    private int price;
+    private int quantity;
+    private int maxPrice1;
+    private int maxPrice2;
+
+    public Product() {
+    }
+
+    public Product(String name, int quantity) {
+        this.name = name;
+        this.quantity = quantity;
+    }
+
+    public Product(String name, int price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public static void main(String[] args) {
+        Product product = new Product();
+
+        Product item1 = new Product("item1", 10, 1);
+        Product item2 = new Product("item2", 20, 2);
+        Product item3 = new Product("item3", 30, 3);
+        Product item4 = new Product("item4", 40, 4);
+
+        if (item1.price > item2.price) {
+            Product maxPrice1 = new Product(item1.name, item1.quantity);
+            product.maxPrice1 = item1.price;
+        } else {
+            Product maxPrice1 = new Product(item2.name, item2.quantity);
+            product.maxPrice1 = item2.price;
+        }
+        if (item3.price > item4.price) {
+            Product maxPrice2 = new Product(item3.name, item3.quantity);
+            product.maxPrice2 = item1.price;
+        } else {
+            Product maxPrice2 = new Product(item4.name, item4.quantity);
+            product.maxPrice2 = item2.price;
+        }
+        if (product.maxPrice1 > product.maxPrice2) {
+            System.out.println(maxPrice1);
+        } else {
+            System.out.println(maxPrice2);
+        }
+    }
+}
