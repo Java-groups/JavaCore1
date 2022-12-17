4d0eff425cf53209f058d9c3e478d8fd0867546c
olesmack <oles.mack@gmail.com>
2022-12-17 16:39:50 +0200

    Dog fixes

--- com/softserve/hw4/Dog.java
+++ com/softserve/hw4/Dog.java
@@ -3,53 +3,49 @@ package com.softserve.hw4;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Objects;
 
 public class Dog {
 
     public static void main(String[] args) throws IOException {
+        Dog dog = new Dog();
         Dog dog1 = new Dog();
         Dog dog2 = new Dog();
         Dog dog3 = new Dog();
+
         dog1.setName();
         dog1.setAge();
-        dog1.checkDogNames();
-        dog1.oldestDogNameAndBreed();
 
         dog2.setName();
         dog2.setAge();
-        dog2.checkDogNames();
-        dog2.oldestDogNameAndBreed();
 
         dog3.setName();
         dog3.setAge();
-        dog3.checkDogNames();
-        dog3.oldestDogNameAndBreed();
+
+        dog.compareDogNames();
+        dog.oldestDogNameAndBreed();
     }
 
     private String name;
-    private String breed;
+    private Breed breed;
     private int age;
-    private int i = 1;
-
-    public void Dog(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
 
     public Dog() {
 
     }
 
     public Dog(String name) {
+        this.name = name;
     }
 
-    public Dog(int age) {
+    public Dog(int age, String name, Breed breed) {
+        this.age = age;
+        this.breed = breed;
     }
 
     public enum Breed {
         JACK_RUSSELL, WHITE_TERRIER, SAMOYED;
     }
-
 
     public void setName() throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
@@ -63,22 +59,39 @@ public class Dog {
         age = Integer.parseInt(bf.readLine());
     }
 
-    public void checkDogNames() {
+    public void compareDogNames() {
+        Dog dog;
+
         Dog dog1 = new Dog(name);
         Dog dog2 = new Dog(name);
         Dog dog3 = new Dog(name);
-        if (dog1.equals(dog2) || dog1.equals(dog3) || dog2.equals(dog3)) {
-            System.out.println("There is 2 same dog's names: " + name);
+
+        if (dog1.equals(dog2)) {
+            dog = dog1;
+            System.out.println(dog);
+        }
+        if (dog1.equals(dog3)) {
+            dog = dog1;
+            System.out.println(dog);
+        }
+        if (dog2.equals(dog3)) {
+            dog = dog2;
+            System.out.println(dog);
         }
     }
 
     public void oldestDogNameAndBreed() {
-        Dog dog1 = new Dog(age);
-        Dog dog2 = new Dog(age);
-        Dog dog3 = new Dog(age);
+        Breed breed1 = Breed.JACK_RUSSELL;
+        Breed breed2 = Breed.SAMOYED;
+        Breed breed3 = Breed.WHITE_TERRIER;
+
+        Dog dog1 = new Dog(age, name, breed1);
+        Dog dog2 = new Dog(age, name, breed2);
+        Dog dog3 = new Dog(age, name, breed3);
+
         maxAge(dog1, dog2, dog3);
-        maxAge(dog2, dog3, dog1);
-        maxAge(dog3, dog1, dog2);
+        maxAge(dog2, dog1, dog3);
+        maxAge(dog3, dog2, dog1);
     }
 
     public void maxAge(Dog dog1, Dog dog2, Dog dog3) {
@@ -92,9 +105,22 @@ public class Dog {
 
     @Override
     public String toString() {
-        return "Dog" +
+        return "Dog " +
                 "name='" + name + '\'' +
                 ", breed='" + breed + '\'' +
                 ", age=" + age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Dog dog = (Dog) o;
+        return age == dog.age && Objects.equals(name, dog.name) && breed == dog.breed;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, breed, age);
     }
 }
