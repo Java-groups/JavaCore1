0a1d75467ad649b82a8d6a5054fac1a5e31d863a
arthurmkrtchian <mkrtchian.arthur999@gmail.com>
2022-12-20 22:51:14 +0200

    Added practical tasks from OOP 2 section
     Changes to be committed:
    	new file:   src/arthurmkrtchian/javaCore/OOP2/practicalTasks/practicalTask1/Animal.java
    	new file:   src/arthurmkrtchian/javaCore/OOP2/practicalTasks/practicalTask1/Cat.java
    	new file:   src/arthurmkrtchian/javaCore/OOP2/practicalTasks/practicalTask1/Dog.java
    	new file:   src/arthurmkrtchian/javaCore/OOP2/practicalTasks/practicalTask1/Main.java
    	new file:   src/arthurmkrtchian/javaCore/OOP2/practicalTasks/practicalTask2/Cleaner.java
    	new file:   src/arthurmkrtchian/javaCore/OOP2/practicalTasks/practicalTask2/Main.java
    	new file:   src/arthurmkrtchian/javaCore/OOP2/practicalTasks/practicalTask2/Person.java
    	new file:   src/arthurmkrtchian/javaCore/OOP2/practicalTasks/practicalTask2/Staff.java
    	new file:   src/arthurmkrtchian/javaCore/OOP2/practicalTasks/practicalTask2/Student.java
    	new file:   src/arthurmkrtchian/javaCore/OOP2/practicalTasks/practicalTask2/Teacher.java

---
+++ src/arthurmkrtchian/javaCore/OOP2/practicalTasks/practicalTask2/Person.java
@@ -0,0 +1,7 @@
+package arthurmkrtchian.javaCore.OOP2.practicalTasks.practicalTask2;
+
+public abstract class Person {
+    public String name;
+
+    public abstract void print();
+}
---
+++ src/arthurmkrtchian/javaCore/OOP2/practicalTasks/practicalTask2/Teacher.java
@@ -0,0 +1,20 @@
+package arthurmkrtchian.javaCore.OOP2.practicalTasks.practicalTask2;
+
+public class Teacher extends Staff {
+
+    private final String TYPE_PERSON = "Teacher";
+
+    public Teacher() {
+        System.out.println(TYPE_PERSON);
+    }
+
+    @Override
+    public double salary() {
+        return 100;
+    }
+
+    @Override
+    public void print() {
+        System.out.println("Hi, I am a teacher!");
+    }
+}
