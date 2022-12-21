ed2a3765d9b3db88e00336b6d5a83283594e3553
PolinaChebukina <polina.chebukina@gmail.com>
2022-12-21 19:10:05 +0200

    Test hw 6

---
+++ src/test/HW6/com/HW6Test.java
@@ -0,0 +1,36 @@
+package HW6.com;
+
+import HW6.com.model.passangers.flyingVehicle.Helicopter;
+import HW6.com.model.passangers.waterVehicle.Liner;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class HW6Test {
+    Liner liner = new Liner(5);
+
+
+
+
+    @Test
+    public void getPassangers(){
+        assertTrue(liner.getPassangers() == 5);
+
+    }
+
+    @Test
+    public void getFloors(){
+        liner.setFloors(7);
+        assertTrue(liner.getFloors() == 5);
+
+    }
+
+    @Test
+    public void isSailing(){
+        liner.setFloors(15);
+        String isSailing = "I am not sailing I have " + liner.getFloors() + " floors" + " and can have "+ liner.getPassangers()+ " passangers";
+        String testString = "I am not sailing I have 15 floors and can have 5 passangers";
+        assertTrue(isSailing.equals(testString));
+    }
+}
