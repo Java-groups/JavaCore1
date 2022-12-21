19fd1047137ba99c50efde6af6b49bb038c731da
olesmack <oles.mack@gmail.com>
2022-12-21 22:17:32 +0200

    fixes

--- com/softserve/hw4/Car/CarService.java
+++ com/softserve/hw4/Car/CarService.java
@@ -5,8 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
-public class CarService extends Car{
-//    Car car = new Car();
+public class CarService extends Car {
 
     BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 
