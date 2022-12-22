39ae3b7698a8fab4afff2b8ab6145aeb461dbc10
arthurmkrtchian <mkrtchian.arthur999@gmail.com>
2022-12-23 00:26:56 +0200

    Added homework and practical task from Nested Classes section
     Changes to be committed:
    	new file:   src/arthurmkrtchian/javaCore/nestedClasses/homeWorks/homework/FullName.java
    	new file:   src/arthurmkrtchian/javaCore/nestedClasses/homeWorks/homework/Main.java
    	new file:   src/arthurmkrtchian/javaCore/nestedClasses/homeWorks/homework/Person.java
    	new file:   src/arthurmkrtchian/javaCore/nestedClasses/homeWorks/homework/Student.java
    	new file:   src/arthurmkrtchian/javaCore/nestedClasses/practicalTasks/practicalTask/Department.java
    	new file:   src/arthurmkrtchian/javaCore/nestedClasses/practicalTasks/practicalTask/Main.java

---
+++ src/arthurmkrtchian/javaCore/nestedClasses/homeWorks/homework/FullName.java
@@ -0,0 +1,48 @@
+package arthurmkrtchian.javaCore.nestedClasses.homeWorks.homework;
+
+//        Develop a FullName class with the firstName and lastName fields of type String, which
+//        would correspond to the principle of encapsulation.
+//        • Create an abstract Person class with fullName field of type FullName and age of type int.
+//        • In the Person class, create a constructor
+//        public Person(FullName fullName, int age)
+//        and a method called info(), which will return a string in the format
+//        "First name: <firstName>, Last name: <lastName>, Age: <age>"
+//        and an abstract public activity() method with a String return type.
+//
+//        Develop a Student class with an int field that matches the course the student is taking.
+//        • In the Student class, create a constructor with parameters to initialize all fields in the
+//        class, and override the info() method (which would also add course information to the
+//        previous line), and the activity() method from the Person class. The activity() method
+//        should return a string value that is the type of activity for the corresponding Person
+//        subtype, for example for a student - this could be the value "I study at university".
+//        • In the main(...) method, create two instances of the Student class and output information
+//        about them by calling the appropriate methods info() and activity()
+//        • Create one more instance of Student class by cloning the first student,
+//        change a course for this object and output full information about all of
+//        created students
+
+public class FullName {
+    private String firstName;
+    private String lastName;
+
+    public FullName(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
