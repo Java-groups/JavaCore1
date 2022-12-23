c715ba77acc82a6769c032206980e2362893b847
StepanBilashuk <stpn.bilashuk@gmail.com>
2022-12-23 18:04:55 +0200

    Added HomeWork task in HomeWork8

---
+++ HomeWork8/MyCollection/Main.java
@@ -0,0 +1,19 @@
+package edu.homeworks.hw8.homework8;
+
+
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        MyCollection myCollection = new MyCollection();
+        myCollection.randomNumbers();
+        myCollection.swapMinMax();
+        myCollection.addZero();
+        myCollection.swapElements();
+        myCollection.copyElementsToLists();
+        myCollection.removeLastEvenElement();
+        myCollection.removeLastMinimalElement();
+
+
+    }
+}
