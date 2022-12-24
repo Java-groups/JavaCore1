f8436204bb9d90c85e3b98d3c214442d50227fa4
Roman Kukliak <roma.kukliak@gmail.com>
2022-12-24 22:59:44 +0200

    Adding PracticalTask2

---
+++ HM7/PracticalTask2/Staff.java
@@ -0,0 +1,12 @@
+package HM7.PracticalTask2;
+
+public abstract class Staff extends Person{
+
+    public Staff(String name) {
+        super(name);
+    }
+
+    String salary() {
+        return null;
+    }
+}
---
+++ HM7/PracticalTask2/Teacher.java
@@ -0,0 +1,19 @@
+package HM7.PracticalTask2;
+
+public class Teacher extends Staff{
+    static final String TYPE_PERSON = "teacher";
+
+    Teacher(String name) {
+        super(name);
+    }
+
+    @Override
+    String print() {
+        return "My name is " + name + ". I am a " + TYPE_PERSON;
+    }
+
+    @Override
+    String salary() {
+        return "My salary is $2000.00";
+    }
+}
