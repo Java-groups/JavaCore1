1066b002261c0758e2d61d652d3c74afb8bffdb2
Roman Kukliak <roma.kukliak@gmail.com>
2022-12-25 08:55:38 +0200

    Added HM7 (one task is missing yet)

---
+++ HM7/HomeWorkTask/FullName.java
@@ -0,0 +1,26 @@
+package HM7.HomeWorkTask;
+
+public class FullName {
+    private String firstName, lastName;
+
+    FullName(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
---
+++ HM7/HomeWorkTask/Student.java
@@ -0,0 +1,25 @@
+package HM7.HomeWorkTask;
+
+public class Student extends Person implements Cloneable{
+    int courseId;
+
+    public Student(FullName fullName, int age, int courseId) {
+        super(fullName, age);
+        this.courseId = courseId;
+    }
+
+    @Override
+    String activity() {
+        return "I study at university";
+    }
+
+    @Override
+    String info() {
+        return super.info() + ", Course ID: " + courseId;
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+}
