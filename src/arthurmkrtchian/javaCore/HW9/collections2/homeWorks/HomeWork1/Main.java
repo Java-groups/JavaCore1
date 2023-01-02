1e9e96472e56b5f25652d7a11a10d885d6320e55
arthurmkrtchian <mkrtchian.arthur999@gmail.com>
2023-01-02 17:11:56 +0200

    Added first and second homeworks from Collection 2 section
    	new file:   src/arthurmkrtchian/javaCore/HW9/collections2/homeWorks/HomeWork1/Main.java
    	new file:   src/arthurmkrtchian/javaCore/HW9/collections2/homeWorks/HomeWork2/Main.java
    	new file:   src/arthurmkrtchian/javaCore/HW9/collections2/homeWorks/homeWork3/Main.java
    	modified:   src/helper/DataTypes.java

---
+++ src/arthurmkrtchian/javaCore/HW9/collections2/homeWorks/HomeWork1/Main.java
@@ -0,0 +1,51 @@
+package arthurmkrtchian.javaCore.HW9.collections2.homeWorks.HomeWork1;
+
+
+//        1. Write parameterized methods union(Set set1, Set set2) and intersect(Set set1, Set set2),
+//          realizing the operations of union and intersection of two sets. Test the operation of
+//          these techniques on two pre-filled sets.
+
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>();
+
+        fillSet(set1,10);
+        System.out.print("       First Set -> ");
+        System.out.println(set1.toString());
+
+        fillSet(set2,10);
+        System.out.print("      Second Set -> ");
+        System.out.println(set2.toString());
+
+        System.out.print("     United Sets -> ");
+        System.out.println(union(set1,set2));
+
+        System.out.print("Intersected Sets -> ");
+        System.out.println(intersect(set1,set2));
+    }
+
+    public static void fillSet(Set<Integer> set, int countOfElements){
+        Random random = new Random();
+        while(set.size() < countOfElements){
+            set.add(random.nextInt(20));
+        }
+    }
+
+    public static  <T> Set<T> union(Set<T> set1, Set<T> set2){
+        Set<T> result = new HashSet<>(set1);
+        result.addAll(set2);
+        return result;
+    }
+
+    public static <T> Set<T> intersect(Set<T> set1, Set<T> set2){
+        Set<T> result = new HashSet<>(set1);
+        result.retainAll(set2);
+        return result;
+    }
+
+}
---
+++ src/arthurmkrtchian/javaCore/HW9/collections2/homeWorks/homeWork3/Main.java
@@ -0,0 +1,19 @@
+package arthurmkrtchian.javaCore.HW9.collections2.homeWorks.homeWork3;
+
+
+//        3. Write class Student that provides information about the name of the student and his
+//          course. Class Student should consist of
+//          a) properties for access to these fields
+//          b) constructor with parameters
+//          c) method printStudents (List students, Integer course), which receives a list of students
+//          and the course number and prints to the console the names of the students from
+//          the list, which are taught in this course (use an iterator)
+//        d) methods to compare students by name and by course
+//        e) In the main() method
+//          • declare List students and add to the list five different students
+//          • display the list of students ordered by name
+//          • display the list of students ordered by course.
+
+
+public class Main {
+}
