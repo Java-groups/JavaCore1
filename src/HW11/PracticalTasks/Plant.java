1155691fca4ec0ccfe95fef1402ff4fec41388cc
Serhii Bodnar <serhiivbodnar@gmail.com>
2023-01-09 11:22:15 +0200

    added HW11 (Practical Tasks some fixes)

--- src/HW11/PracticalTasks/Plant.java
+++ src/HW11/PracticalTasks/Plant.java
@@ -62,28 +62,44 @@ public class Plant {
                 '}';
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ColorException, TypeException {
         Plant[] plants = new Plant[5];
         try {
             plants[0] = new Plant(4, "green", "trees");
             System.out.println(plants[0]);
+        } catch (ColorException | TypeException e) {
+            System.err.println(e.getMessage() + "\n");
+        }
+        try {
             plants[1] = new Plant(3, "red", "shrubs");
             System.out.println(plants[1]);
-            plants[2] = new Plant(1,"red","herbs");
+        } catch (ColorException | TypeException e) {
+            System.err.println(e.getMessage() + "\n");
+        }
+        try {
+            plants[2] = new Plant(1, "red", "herbs");
             System.out.println(plants[2]);
+        } catch (ColorException | TypeException e) {
+            System.err.println(e.getMessage() + "\n");
+        }
+        try {
             plants[3] = new Plant(5, "black", "tress");
             System.out.println(plants[3]);
-            plants[4] = new Plant(2,"red", "rocks");
+        } catch (ColorException | TypeException e) {
+            System.err.println(e.getMessage() + "\n");
+        }
+        try {
+            plants[4] = new Plant(2, "red", "rocks");
             System.out.println(plants[4]);
+        } catch (ColorException | TypeException e) {
+            System.err.println(e.getMessage() + "\n");
+        }
 
 //            for (int i = 0; i < plants.length; i++) {
 //                System.out.println(plants[i]);
 //            }
-
-        } catch (ColorException | TypeException e) {
-            System.err.println(e.getMessage() + "\n");
-        }
         System.out.println("end");
+
 
     }
 }
