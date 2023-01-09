ca35a42d9140d18319af1026eb8f85f03c409f57
PolinaChebukina <polina.chebukina@gmail.com>
2023-01-09 21:48:00 +0200

    PracticalTask +HW12, for the first task in HW i used ASCII code PolinaChebukina

--- src/HW12/HW12.java
+++ src/HW12/HW12.java
@@ -7,23 +7,30 @@ import java.util.Date;
 import java.util.Scanner;
 
 public class HW12 {
+    public static final int START_ASCII = 97;
+    public static final int END_ASCII = 122;
 
     public static void main(String[] args) {
+
         System.out.println("Start of first task");
         System.out.println("Enter string of letters:");
         Scanner sc = new Scanner(System.in);
         sc = new Scanner(System.in);
         String str = sc.nextLine();
-        changeString(str);
+        String encryptedMessage = encrypt(str.toLowerCase(), 3);
+        decrypt(encryptedMessage, 3);
+
         System.out.println("Start of second task");
         System.out.println("Enter date:");
         sc = new Scanner(System.in);
         String value = sc.nextLine();
         System.out.println(testDate(value));
+
         System.out.println("Start of third task");
         System.out.println("Enter year:");
         int year = sc.nextInt();
         leapYear(year);
+
         System.out.println("Start of fourth task");
         LocalDate birthday = LocalDate.of(2004, 12, 10);
         printDays(birthday);
@@ -33,21 +40,35 @@ public class HW12 {
 
 
     }
-    static void changeString(String str) {
-        char[] charArray = str.toCharArray();
+
+    static String encrypt(String str, int shift) {
+        char[] encryptArray = new char[str.length()];
         for (int i = 0; i < str.length(); i++) {
-            int distance = charArray[i] - '2';
-            if (distance + (charArray[i] - '2') > 26) {
-                distance = (distance + (charArray[i] - '2')) % 26;
-                charArray[i] = (char) ('2' + distance);
+            if ((str.charAt(i) + shift) > END_ASCII) {
+                encryptArray[i] = (char) ((str.charAt(i) + shift) - END_ASCII + START_ASCII - 1);
+            } else {
+                encryptArray[i] = (char) ((str.charAt(i) + shift));
             }
-            else {
-                charArray[i] = (char) (charArray[i] + distance);
+
+        }
+        String encryptedString = new String(encryptArray);
+        System.out.println("Encrypting..." + encryptedString);
+        return encryptedString;
+    }
+
+    static void decrypt(String str, int shift) {
+        char[] decryptedArray = new char[str.length()];
+        for (int i = 0; i < str.length(); i++) {
+            if ((str.charAt(i) - shift) < START_ASCII) {
+                decryptedArray[i] = (char) (END_ASCII - Math.abs(str.charAt(i) - shift - START_ASCII + 1));
+            } else {
+                decryptedArray[i] = (char) ((str.charAt(i) - shift));
             }
+
         }
 
-        String string = new String(charArray);
-        System.out.println(string.toLowerCase());
+        String string = new String(decryptedArray);
+        System.out.println("Decrypting..." + string);
     }
 
     public static void leapYear(int year) {
--- src/HW12/PracticalTask.java
+++ src/HW12/PracticalTask.java
@@ -15,43 +15,47 @@ import java.util.stream.Collectors;
 
 public class PracticalTask {
     public static void main(String[] args) {
+
         System.out.println("Start of first practical task");
         System.out.println(LocalDate.now());
+
         System.out.println("Start of second practical task");
-        LocalDate localDate = LocalDate.of(2023, Month.JANUARY,13);
+        LocalDate localDate = LocalDate.of(2023, Month.JANUARY, 13);
         TemporalAdjuster firstMonInJan = TemporalAdjusters.firstInMonth(DayOfWeek.MONDAY);
         System.out.println(localDate.with(firstMonInJan));
+
         System.out.println("Start of third practical task");// first is for array and second for list
-        Seasons[] season = { new Seasons("Winter"), new Seasons("Spring"),
-                new Seasons("Summer"), new Seasons("Autumn") };
-        System.out.println("Before sorting"+ season);
+        Seasons[] season = {new Seasons("Winter"), new Seasons("Spring"),
+                new Seasons("Summer"), new Seasons("Autumn")};
+        System.out.println("Before sorting" + season);
         Arrays.sort(season, Seasons::compareByName);
-        System.out.println("After sorting"+ season);
+        System.out.println("After sorting" + season);
         System.out.println(Arrays.toString(season));
         List<String> names =
                 Arrays.asList("Angelina", "Anna", "Anastasia", "Anabel", "Polina");
         System.out.println(names);
         List<String> namesWithA = names.stream().filter(name -> name.startsWith("A")).collect(Collectors.toList());
         System.out.println(namesWithA);
+
         System.out.println("Start of fourth practical task");
-        Integer[] numbers = new Integer[]{1,2,34,5,6,7,8,9,0};
-        int count = count(numbers,n -> n > 8);
-        System.out.println("The  count of odd numbers in array: "+ count);
-        }
+        Integer[] numbers = new Integer[]{1, 2, 34, 5, 6, 7, 8, 9, 0};
+        int count = count(numbers, n -> n > 8);
+        System.out.println("The  count of odd numbers in array: " + count);
+    }
 
-        public static int count(Integer[] array, Predicate<Integer> predicate){
+    public static int count(Integer[] array, Predicate<Integer> predicate) {
         int count = 0;
-            for (int i = 0; i < array.length; i++) {
-                if (predicate.test(array[i])){
-                    count++;
-                }
+        for (int i = 0; i < array.length; i++) {
+            if (predicate.test(array[i])) {
+                count++;
             }
-            return count;
         }
+        return count;
+    }
 
-        public static int sum(Integer[] array, Function<Integer[], Integer> function){
-        int result = 0 ;
+    public static int sum(Integer[] array, Function<Integer[], Integer> function) {
+        int result = 0;
 
         return result;
-        }
+    }
 }
