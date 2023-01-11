91e879d1fb8ee5df93b3380ce3725d226442e222
PolinaChebukina <polina.chebukina@gmail.com>
2023-01-11 09:26:50 +0200

    fixes

--- src/HW11/HW11.java
+++ src/HW11/HW11.java
@@ -3,6 +3,7 @@ package HW11;
 import HW11.model.CheckRangeException;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -61,13 +62,14 @@ public class HW11 {
         int start = sc.nextInt();
         System.out.println("Enter end: ");
         int end = sc.nextInt();
-        System.out.println("Enter 10 numbers [1;100]");
+        System.out.println("Enter 10 numbers [1;100] growing up in order");
 
         for (int i = 0; i < 10; i++) {
 
-            readNumber(start, end);
+            readNumber(start,end);
 
         }
+
     }
 
 
