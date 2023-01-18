da0029f1da806536847fe465f02d582322f1331e
KaterynaIefimova <KatiaE@ukr.net>
2023-01-19 00:08:31 +0200

    add HW10

--- src/HW10/Task1.java
+++ src/HW10/Task1.java
@@ -3,7 +3,6 @@ import java.io.BufferedReader;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 
 /* Enter in the console sentence of five words.
          display the longest word in the sentence
