efdadffa4edf37e653cdbe8d504390bfd97dfce9
Serhii Bodnar <serhiivbodnar@gmail.com>
2023-01-23 10:42:23 +0200

    5 task

--- src/Test.java
+++ src/Test.java
@@ -9,15 +9,22 @@ public class Test {
 
         int option = 0;
         do {
+            System.out.println("Enter task number:");
             option = Integer.parseInt(br.readLine());
             switch (option) {
                 case 1:
 
 
                 case 2:
+                case 5:
+                    System.out.println("1 dollar costs 40 hryvnias");
+                    System.out.println("Enter amount of dollars:");
+                    double dollars = Integer.parseInt(br.readLine());
+                    double hryvnias = dollars*40;
+                    System.out.println("It is " + hryvnias + " hryvnias");
                     while (option != 3) ;
 
             }
-        } while (option!=15);
+        } while (option != 15);
     }
 }
